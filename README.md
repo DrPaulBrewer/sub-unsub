332af2d62e1b0b05826cb714a3df67442813a08c
Dr Paul Brewer <drpaulbrewer@eaftc.com>
2018-12-06 02:30:44 +0000

    rewrite to poll fastspring for an active subscription to a product from a configurable list -- drop backend notification processor and other fluff

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ A Hapi plugin for sites with paid user subscriptions via fastspring
 ### Compatibility
 
 * sub-unsub will eventually work with hapi 16
-* this software simply doesn't work yet (Oct. 2018) and needs reorganization
-* so you really shouldn"t be using this yet...
+* this software is untested (Dec. 2018) and probably doesn't work yet
+* so you really shouldnt be using this unless you intend to help out...
 
 ### Installation
 
@@ -19,50 +19,19 @@ A Hapi plugin for sites with paid user subscriptions via fastspring
 #### ./path/to/hapi/config/sub-unsub
 
     {
-      "hexidSecret": "akfjalkfjasdlkfjasdlkfjasd",
-    	"signatureSecret": "dfkajflkajdflkajflkajdlkfjaklf",
-    	"subscriptionDB": "http://your-couchdb-ip-address:5984/",
-      "handledEventTypes":[
-        "subscription.activated",
-        "subscription.deactivated",
-        "subscription.updated",
-        "subscription.cancelled"
-      ],
-      "fields": {
-        "date": [
-          "changed",
-          "next",
-          "end",
-          "canceledDate",
-          "deactivationDate",
-          "begin",
-          "nextChargeDate",
-          "nextNotificationDate"
-        ],
-        "string":[
-          "id",
-          "state",
-          "sku",
-          "display"
-        ],
-        "boolean":[
-          "active",
-          "live",
-          "adhoc",
-          "autoRenew"
-        ]
+		fsurl: "https://user:password@api.fastspring.com/",
+		fsproduct: ["bronze","silver","gold"]
     }
-  }
 
 
 **Note: this will also require some set up in Fastspring before it will work**
 
 ## Features
-
-* provides the backend webhook for Fastspring
-* update a pouchDB/couchDB database from the Fastspring subscription updates  
-* anonymizes emails to hexids using a security string
-* provides `req.sub` on all requests containing most recent subscription update for Google Sign In user
+* uses req.drive, set by npm:Bugle, to obtain user's email address as registered with Google Accounts
+* sanity checks email
+* inquires with Fastspring if an account exists for email
+* inquires with Fastspring for an active subscription associated with account that matches an entry in fsproduct array
+* provides `req.fastspring` on all requests. May contain these properties: error, acct, subid, sub
 
 ## Copyright
 
--- index.js
+++ index.js
@@ -1,128 +1,55 @@
 /* eslint-disable no-console */
 
-const crypto = require('crypto');
-const pFilter = require('p-filter');
 const Boom = require('boom');
-const PouchDB = require('pouchdb-node');
-const Joi = require('joi');
-PouchDB.plugin(require('pouchdb-upsert'));
-
-function removeEmpty(obj){
-  // recusively remove null properties from obj and nested objects
-  // See Rotareti's answer on Stack Overflow
-  // https://stackoverflow.com/a/38340730/103081
-  Object.entries(obj).forEach(([key, val]) => {
-    if (val && typeof val === 'object') removeEmpty(val);
-    else if (val === null) delete obj[key];
-  });
-}
+const axios = require('axios');
+const pLocate = require('p-locate');
 
 function subUnsub(server, options, next) {
-  if (!options.hexidSecret) throw Boom.badImplementation("options.hexidSecret missing in sub-unsub configuration");
-  if (!options.signatureSecret) throw Boom.badImplementation("options.signatureSecret missing in sub-unsub configuration");
-  if (!options.subscriptionDB) throw Boom.badImplementation("options.subscriptionDB missing in sub-unsub configuration");
-
-  const db = new PouchDB(options.subscriptionDB);
-
-  function hasValidSignature(req) {
-    const secret = options.signatureSecret;
-    const reqSignature = req.headers[options.signatureHeader];
-    if (!reqSignature) return false;
-    const bodySignature = (
-      crypto
-      .createHmac('sha256', secret)
-      .update(req.payload)
-      .digest()
-      .toString('base64')
-    );
-    return (reqSignature === bodySignature);
-  }
-
-  function hexidFor(email) {
-    const secret = options.hexidSecret;
-    const standardizedEmail = email.toLowerCase().trim();
-    return (
-      crypto
-      .createHmac('sha256', secret)
-      .update(standardizedEmail, 'utf8')
-      .digest('hex')
-    );
-  }
-
-  const schema = {};
-  options.fields.date.forEach((k) => {
-    schema[k] = Joi.date().timestamp('javascript').optional(); // eslint-disable-line newline-per-chained-call
-  });
-  options.fields.string.forEach((k) => {
-    schema[k] = Joi.string().optional();
-  });
-  options.fields.boolean.forEach((k) => {
-    schema[k] = Joi.boolean().optional();
-  });
-
-  // in .addons[i]
-
-  schema.addons = Joi.array().items(
-    Joi.object().keys({
-      sku: Joi.string().optional(),
-      display: Joi.string().optional(),
-      quantity: Joi.number().integer()
-    })
-  );
-
-  const joiOptions = {
-    allowUnknown: true,
-    skipFunctions: true,
-    stripUnknown: true
-  };
-
-  async function task(eventType, data) {
-    try {
-      // TODO: If data.account is a string, ask fastspring api for email and await response
-      const email = data.account.contact.email;
-      const hexid = hexidFor(email);
-      // flatten data object to include any subscription fields
-      if (typeof(data.subscription) === 'object') {
-        Object.assign(data, data.subscription);
-        delete data.subscription;
-      }
-      const save = await Joi.validate(data, schema, joiOptions);
-      if (save) {
-        if (save.id) {
-          save.fsid = save.id;
-          delete save.id;
-        }
-        save.eventType = eventType;
-        save.eventReceived = Date.now();
-        save._id = hexid; // eslint-disable-line no-underscore-dangle
-        if (options.handledEventTypes.includes(eventType)) {
-          if (save.live) {
-            const result = await db.upsert(hexid, (indata) => (Object.assign({}, indata, save)));
-            return result.updated;
-          }
-          console.log(new Date().toUTCString() + " received test event:"); // eslint-disable-line no-console
-          console.log(JSON.stringify(save, null, 2)); // eslint-disable-line no-console
-          return true;
-        }
-      }
-    } catch (e) {
-      console.log(`error processing Fastspring event ${eventType}, event unprocessed`); // eslint-disable-line no-console
-      console.log(e); // eslint-disable-line no-console
-    }
-    return false;
-  }
+  if (!options.fsurl) throw Boom.badImplementation("options.fsurl missing in sub-unsub configuration");
+  if (!Array.isArray(options.fsproduct)) throw Boom.badImplementation("options.fsproduct array missing in sub-unsub configuration");
 
   async function getGoogleDriveAssociatedSubscription(req, reply) {
+    req.fastspring = {};
     if (req.drive) {
       try {
         const info = await req.drive.x.aboutMe();
         const email = info.user.emailAddress;
-        const hexid = hexidFor(email);
-        const sub = await db.get(hexid, { latest: true });
-        req.sub = sub;
+        if ((!email) || !(email.length)) throw new Error("no email");
+        if (email.indexOf('@')<0) throw new Error("email missing @ symbol");
+        if ((email.startsWith('@')) || (email.endsWith('@'))) throw new Error("invalid email: "+email);
+        const fsAccounts = await axios.get(options.fsurl+"accounts", {email});
+        if (fsAccounts.result !== "success") throw new Error("no fastspring account");
+        if ((!fsAccounts.accounts) || (!fsAccounts.accounts.length)) throw new Error("missing fastspring account");
+        if (fsAccounts.accounts.length > 1) throw new Error("multiple fastspring accounts");
+        const fsAccount = fsAccounts.accounts[0];
+        req.fastspring.acct = fsAccount;
+        const subids = fsAccount.subscriptions;
+        if (subids && subids.length){
+          req.fastspring.subid = await pLocate(
+            subids,
+            (id)=>(
+              axios
+                .get(options.fsurl+"subscriptions/"+id)
+                .then((sub)=>{
+                  if (
+                    (sub.active) &&
+                    (typeof(sub.product)==="string") &&
+                    (options.fsproduct.includes(sub.product))
+                  ){
+                    req.fastspring.sub = sub;
+                    return true;
+                  }
+                  return false;
+                  }
+                )
+              )
+            );
+          }
       } catch (e) {
-        console.log("Error in getGoogleDriveAssociatedSubscription: " + e.toString()); // eslint-disable-line no-console
+        req.fastspring.error = e;
       }
+    } else {
+      req.fastspring.error = new Error("not signed in");
     }
     reply.continue();
   }
@@ -130,43 +57,6 @@ function subUnsub(server, options, next) {
   server.ext([{
     type: 'onPreAuth',
     method: getGoogleDriveAssociatedSubscription
-  }]);
-
-  server.route([{
-    path: options.webhookPath,
-    method: 'POST',
-    config: {
-        payload: {
-            output: 'data',
-            parse: false
-        }
-    },
-    handler(req, reply) {
-      if (!hasValidSignature(req)) {
-        console.log("invalid signature");
-        return reply(Boom.badRequest("invalid signature"));
-      }
-      try {
-        req.payload = JSON.parse(req.payload);
-      } catch(e){
-        console.log("JSON.parse failed: "+e);
-        return reply(Boom.badRequest("invalid JSON"));
-      }
-      removeEmpty(req.payload);
-      if (!Array.isArray(req.payload.events)) {
-        console.log("missing events");
-        return reply(Boom.badRequest("missing events"));
-      }
-      return (
-        pFilter(
-          req.payload.events,
-          (event) => (task(event.type, event.data)), { concurrency: 1 }
-        )
-        .then((events) => (events.map((event) => (event.id))))
-        .then((ids) => (reply(ids)))
-        .catch((e)=>(console.log("Error processing "+options.webhookPath+" :"+e))) // eslint-disable-line no-console
-      );
-    }
   }]);
 
   next(); // call next to complete plugin registration
