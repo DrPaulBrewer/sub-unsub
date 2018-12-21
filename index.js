21de8d4f48d47961b307a9207e5721aac41b8f22
Dr Paul Brewer <drpaulbrewer@eaftc.com>
2018-12-21 06:22:46 -0500

    refactor: divide into smaller functions, declare server.dependency on bugle for correct ordering

--- index.js
+++ index.js
@@ -7,64 +7,89 @@ const pLocate = require('p-locate');
 function subUnsub(server, options, next) {
   if (!options.fsurl) throw Boom.badImplementation("options.fsurl missing in sub-unsub configuration");
   if (!Array.isArray(options.fsproduct)) throw Boom.badImplementation("options.fsproduct array missing in sub-unsub configuration");
+  
+  async function validEmail(req){
+    const info = await req.drive.x.aboutMe();
+    const email = info.user.emailAddress.toLowerCase().trim();
+    if ((!email) || !(email.length)) throw new Error("no email");
+    if (email.indexOf('@') < 0) throw new Error("email missing @ symbol");
+    if ((email.startsWith('@')) || (email.endsWith('@'))) throw new Error("invalid email: " + email);
+    return email;    
+  }
+  
+  async function fastspringAccount(email){
+    const fsAccounts = await(
+      axios
+      .get(options.fsurl + "accounts", { params: { email } })
+      .then((r) => (r.data))
+    );
+    if (fsAccounts.result !== "success") throw new Error("no fastspring account");
+    if ((!fsAccounts.accounts) || (!fsAccounts.accounts.length)) throw new Error("missing fastspring account");
+    if (fsAccounts.accounts.length > 1) throw new Error("multiple fastspring accounts");
+    const fsAccount = fsAccounts.accounts[0];
+    return fsAccount;
+  }
+
+  async function findActiveSubscription({ 
+    fsAccount,
+    limit,
+    products
+  }){
+    const result = {};
+    const subids = fsAccount.subscriptions.slice(0,limit);
+    if (subids && subids.length) {
+      result.subid = await pLocate(
+        subids,
+        (id) => (
+          axios
+          .get(options.fsurl + "subscriptions/" + id)
+          .then((response) => (response.data))
+          .then((sub) => {
+            if (sub.active){
+              const isCorrectProduct = (products === undefined) || (products.length === 0) || (
+                (typeof(sub.product) === "string") && (products.includes(sub.product))
+              );
+              if (isCorrectProduct){
+                result.sub = sub;
+                return true;
+              }
+            }
+            return false;
+          })
+        )
+      );
+    }
+    return result;  // { subid: 'fastspring-subscription-id', sub: { fastspring-subscription-object }}
+  }
 
   async function getGoogleDriveAssociatedSubscription(req, reply) {
     req.fastspring = {};
     if (req.drive) {
       try {
-        const info = await req.drive.x.aboutMe();
-        const email = info.user.emailAddress.toLowerCase().trim();
-        if ((!email) || !(email.length)) throw new Error("no email");
-        if (email.indexOf('@')<0) throw new Error("email missing @ symbol");
-        if ((email.startsWith('@')) || (email.endsWith('@'))) throw new Error("invalid email: "+email);
-        const fsAccounts = await(
-          axios
-          .get(options.fsurl+"accounts", {params: {email}})
-          .then((r)=>(r.data))
-        );
-        if (fsAccounts.result !== "success") throw new Error("no fastspring account");
-        if ((!fsAccounts.accounts) || (!fsAccounts.accounts.length)) throw new Error("missing fastspring account");
-        if (fsAccounts.accounts.length > 1) throw new Error("multiple fastspring accounts");
-        const fsAccount = fsAccounts.accounts[0];
+        const email = await validEmail(req);
+        const fsAccount = await fastspringAccount(email);
         req.fastspring.acct = fsAccount;
-        const subids = fsAccount.subscriptions;
-        if (subids && subids.length){
-          req.fastspring.subid = await pLocate(
-            subids,
-            (id)=>(
-              axios
-                .get(options.fsurl+"subscriptions/"+id)
-                .then((response)=>(response.data))
-                .then((sub)=>{
-                  if (
-                    (sub.active) &&
-                    (typeof(sub.product)==="string") &&
-                    (options.fsproduct.includes(sub.product))
-                  ){
-                    req.fastspring.sub = sub;
-                    return true;
-                  }
-                  return false;
-                  }
-                )
-              )
-            );
-          }
+        const result = await findActiveSubscription(fsAccount);
+        Object.assign(req.fastspring, result); 
       } catch (e) {
-          req.fastspring.error = e.toString();
+        req.fastspring.error = e.toString();
       }
     } else {
-	req.fastspring.error = new Error("not signed in").toString();
+      req.fastspring.error = new Error("not signed in").toString();
     }
-    reply.continue();
+    reply.continue(); // continue request processing
   }
 
-  server.ext([{
-    type: 'onPreAuth',
-    method: getGoogleDriveAssociatedSubscription
-  }]);
+  server.dependency('bugle', (S, nextdep)=>{
+    S.ext([{
+      type: 'onPreAuth',
+      method: getGoogleDriveAssociatedSubscription
+    }]);
 
-  next(); // call next to complete plugin registration
+    nextdep(); // complete dependency registration
+  });
+  
+  next();  // complete extension registration
 }
 
 subUnsub.attributes = {
